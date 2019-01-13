66dab613840a8c2f321d97d7e6de2615a341984c
Walter A. Alves <walter.arruda.alves@gmail.com>
2019-01-13 16:25:46 -0200

    Refactor API

--- entrypoint-service/src/main/kotlin/webhook/entrypoint/api/App.kt
+++ entrypoint-service/src/main/kotlin/webhook/entrypoint/api/App.kt
@@ -3,6 +3,7 @@ package webhook.entrypoint.api
 import io.javalin.apibuilder.ApiBuilder.*
 import io.javalin.Javalin
 import mu.KotlinLogging
+import webhook.entrypoint.api.controllers.EntrypointController
 import webhook.entrypoint.communication.Producer
 import webhook.entrypoint.utils.Consts
 import webhook.entrypoint.utils.Environment
@@ -10,58 +11,21 @@ import webhook.entrypoint.utils.http.HttpStatusCode
 import webhook.entrypoint.utils.security.sha1
 
 /**
- * Server main method
+ * Server main class used to startup the Controller
  */
-fun main(args: Array<String>) {
-    val logger = KotlinLogging.logger("api.App")
 
-    logger.info("Starting server on port ${Environment.getServerPort()}")
-    val app = Javalin.create().apply {
-        exception(io.javalin.NotFoundResponse::class.java) { e, ctx ->
-            ctx.status(HttpStatusCode.NOT_FOUND)
-        }
-        exception(Exception::class.java) { e, ctx ->
-            e.printStackTrace()
-            ctx.status(HttpStatusCode.INTERNAL_SERVER_ERROR)
-        }
-    }.start(Environment.getServerPort())
+class Main {
 
-    app.routes{
+    companion object {
+        private  val logger = KotlinLogging.logger(Main::class.java.name)
+        @JvmStatic
+        fun main(args: Array<String>) {
 
-        /**
-         * Endpoint which will receives events from Github
-         */
-        post("/") { ctx ->
-            // Security feature provided by github hooks
-            if(Environment.getGithubSecret() != null){
-                val githubSignature = ctx.header(Consts.GITHUB_SIGNTURE)
-                logger.info("Checking credentials...")
-                if (!sha1(Environment.getGithubSecret()).equals(githubSignature)){
-                    ctx.status(HttpStatusCode.UNAUTHORIZED)
-                }
-                logger.info("Credentials approved!")
-            }
-
-            val githubEventName = ctx.header(Consts.GITHUB_EVENT)
-
-            if(githubEventName != null && githubEventName in Environment.getBrokerQueueWhiteList()){
-                if(githubEventName.equals(Consts.PING)){
-                    ctx.status(HttpStatusCode.OK)
-                    return@post
-                }
-
-                val payload = ctx.body()
-                try {
-                    Producer.publish(githubEventName, payload)
-                    ctx.status(HttpStatusCode.ACCEPTED)
-                }catch(e: Exception){
-                    ctx.status(HttpStatusCode.INTERNAL_SERVER_ERROR)
-                }
-            }else{
-                ctx.status(HttpStatusCode.BAD_REQUET)
-            }
+            logger.info("Initializing web app...")
+            val  app = EntrypointController()
+            app.init()
+            logger.info("Web app initialized!!")
 
         }
-
     }
 }
---
+++ entrypoint-service/src/main/kotlin/webhook/entrypoint/api/controllers/EntrypointController.kt
@@ -0,0 +1,71 @@
+package webhook.entrypoint.api.controllers
+
+import io.javalin.Javalin
+import io.javalin.apibuilder.ApiBuilder
+import mu.KotlinLogging
+import webhook.entrypoint.communication.Producer
+import webhook.entrypoint.utils.Consts
+import webhook.entrypoint.utils.Environment
+import webhook.entrypoint.utils.http.HttpStatusCode
+import webhook.entrypoint.utils.security.sha1
+
+class EntrypointController(){
+    private val logger  = KotlinLogging.logger(EntrypointController::class.java.name)
+
+    fun init(): Javalin {
+        logger.info("Starting server on port ${Environment.getServerPort()}")
+        val app = Javalin.create().apply {
+
+            port(Environment.getServerPort())
+            exception(io.javalin.NotFoundResponse::class.java) { e, ctx ->
+                ctx.status(HttpStatusCode.NOT_FOUND)
+            }
+            exception(Exception::class.java) { e, ctx ->
+                e.printStackTrace()
+                ctx.status(HttpStatusCode.INTERNAL_SERVER_ERROR)
+            }
+        }.start()
+
+        app.routes{
+
+            /**
+             * Endpoint which will receives events from Github
+             */
+            ApiBuilder.post("/") { ctx ->
+                // Security feature provided by github hooks
+                if (Environment.getGithubSecret() != null) {
+                    val githubSignature = ctx.header(Consts.GITHUB_SIGNTURE)
+                    logger.info("Checking credentials...")
+                    if (!sha1(Environment.getGithubSecret()).equals(githubSignature)) {
+                        ctx.status(HttpStatusCode.UNAUTHORIZED)
+                    }
+                    logger.info("Credentials approved!")
+                }
+
+                val githubEventName = ctx.header(Consts.GITHUB_EVENT)
+
+                if (githubEventName != null && githubEventName in Environment.getBrokerQueueWhiteList()) {
+                    if (githubEventName.equals(Consts.PING)) {
+                        ctx.status(HttpStatusCode.OK)
+                        return@post
+                    }
+
+                    val payload = ctx.body()
+                    try {
+                        Producer.publish(githubEventName, payload)
+                        ctx.status(HttpStatusCode.ACCEPTED)
+                    } catch (e: Exception) {
+                        ctx.status(HttpStatusCode.INTERNAL_SERVER_ERROR)
+                    }
+                } else {
+                    ctx.status(HttpStatusCode.BAD_REQUET)
+                }
+
+            }
+
+        }
+
+        return app
+    }
+
+}
